b22f288155c94005f0081c672674ec8608d8bdae
kanyanat999 <kanyanat_khueankham@cmu.ac.th>
2024-12-06 14:52:17 +0700

    lab7 finish

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+    int a = 20, b = 30;  
+    int c = 4;          
+    double output;
+    output = static_cast<double>(a + b) / c;
 
-    outpot = a - b/c;
-    count << output: << output << \n;
+   
+    cout << "output: "<< output << endl;
+
     return 0;
 }
